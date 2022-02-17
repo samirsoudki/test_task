d23f8b1c0f3387835817d746a1535643d36159aa
samirsoudki <78477062+samirsoudki@users.noreply.github.com>
2022-02-17 02:07:52 +0200

    Update is_palindrome.py

--- is_palindrome.py
+++ is_palindrome.py
@@ -13,6 +13,27 @@ Test task
 
 # TODO Write here is_palindrome function 
 
-def is_palindrome():
-    
-    return None
+def Game():
+    word = input("Please write a word or a sentence to check for palindrome\n")
+    def isPalindrome(word):
+        word = word.replace(" ", "")
+        return word == word[::-1]
+    if isPalindrome(word):
+        print("This word is Palindrome")
+    else:
+        print("No, it's not")
+
+start_game = input("Do you want to start the game? \n").lower()
+while True:
+    if start_game == 'yes':
+        Game()
+        restart = input('do you want to restart \n').lower()
+        if restart == 'yes':
+            pass
+        else:
+            print("Goodbye !")
+            break
+    else:
+            print("Goodbye !")
+            break
+
